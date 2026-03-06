6756253d8daedeb0eae12681fef12c0d23f6137a
Nikita Jadhav <nikita.jadhav12022008@gmail.com>
2026-03-06 05:31:44 -0800

    palindrome checker with improved comments
    Updated comments and formatting for clarity.

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -1,6 +1,9 @@
-#palindrome checker:
+#python based mini project the palindrome checker:
+#input the word or a number
 s = input("enter word/number:").lower()
+#conditions
 if(s == s[::-1]):
     print("palindrome")
 else:
   print("not palindrome")
+
